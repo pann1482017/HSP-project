cf1eae9e42f80f0c0bc1d31c26585117eefd47d5
Pann Ei Phyu <panneiphyu5cs43@gmail.com>
2020-10-25 14:10:47 +0630

    first commit

---
+++ logo.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>Logo Marketing</title>
+	<link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="images/Logo-Website/32x32 px - png-logo.png">
+	<style>
+		h5.logo {outline: thick ridge pink;
+		        padding: 3px;
+		        width: 500px;
+		        color: #8D4242;}
+		    ul {
+                    list-style: none;
+                 }
+
+			ul li::before {
+			  content: "\2022";
+			  color: #8D4242;
+			  font-weight: bold;
+			  display: inline-block; 
+			  width: 2em;
+			  margin-left: -1em;
+              }
+              .text-red{ 
+			color: #8D4242;
+
+		}
+	</style>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h5 class="logo my-5 text-center">Logo Marketing</h35>			
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-4">
+				<img src="images/Clients/HSP/Post-32-Logo.png" alt="" class="img-fluid">
+			
+		    </div>
+		    <div class="col-md-8">
+		    	<div class="row mt-5">
+		    	<div class="col-md-1">
+		    		<img src="images/Clients/HSP/About - HSP/Step-1.png" alt="" width="50px;">
+		    	</div>
+		    	<div class="col-md-10">
+		    		<h5 class="text-red">ကျွန်မတို့ရဲ့ HSP Digital Marketing Angency မှ Graphic Designers များက အမိုက်စား Logo တွေကို သူမတူတဲ့ Design ရေးဆွဲပေးနေပါတယ်။</h5>
+		    	</div>
+		        </div>
+		        <div class="row mt-5">
+		    	<div class="col-md-1">
+		    		<img src="images/Clients/HSP/About - HSP/Step-2.png" alt="" width="50px;">
+		    	</div>
+		    	<div class="col-md-10">
+		    		 <h5 class="text-red">Customer များ စိတ်ကြိုက်ဖြစ်စေမယ့် Logo Design ရေးဆွဲပေးနေပါတယ်။</h5>
+		    	</div>
+		        </div>
+
+		    	
+                   <!--  <h5>ကျွန်မတို့ရဲ့ HSP Digital Marketing Angency မှ Graphic Designers များက အမိုက်စား Logo တွေကို သူမတူတဲ့ Design တွေနဲ့ ရေးဆွဲနေပါတယ်။</h5>
+                   <h5>Customer များ စိတ်ကြိုက်ဖြစ်စေမယ့် Logo Design များကို ရေးဆွဲပေးပါတယ်။</h5> -->
+                
+		    </div>
+		</div>
+		
+	</div>
+
+
+<script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+<script src="library/bootstrap/popper.min.js"></script>
+<script src="library/bootstrap/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
